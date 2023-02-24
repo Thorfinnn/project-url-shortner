bef3ccbfb5928a988fa3a02d2032a7120ff08819
Taher Barwaniwala <89532500+Thorfinnn@users.noreply.github.com>
2023-02-24 09:25:20 +0000

    maybe finished project?? need to check in production

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const UrlSchema = new mongoose.Schema({
+  url: String,
+  shortened_url: Number
+});
+
+var Url = new mongoose.model('Url', UrlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +24,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.set('json spaces', 2);
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -22,3 +39,70 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+app.post('/api/shorturl', urlencodedParser, (req, res) => {
+
+  const requrl = req.body.url;
+  try {
+    const url = new URL(requrl);
+    console.log(url.toString());
+    Url.exists({ url: requrl }, (err, dat) => {
+      if (err) return console.log(err);
+      else if (!dat) {
+        console.log(typeof data)
+        Url.find({ shortened_url: { $gte: 0 } }).sort({ shortened_url: -1 }).limit(1).exec((err, data) => {
+          if (err) return console.log(err);
+          urlmodel = new Url({
+            url: requrl,
+            shortened_url: parseInt(data[0].shortened_url) + 1
+          });
+          urlmodel.save((err, d) => {
+            if (err) console.log(err);
+          });
+          return res.json({
+            original_url: requrl,
+            short_url: parseInt(data[0].shortened_url) + 1
+          });
+        });
+      } else {
+        Url.find({ url: requrl }, (err, dat) => {
+          if (err) return console.log(err);
+          else return res.json({
+            original_url: requrl,
+            short_url: parseInt(dat[0].shortened_url)
+          });
+
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      error: "invalid url"
+    })
+  }
+});
+
+app.get('/api/shorturl/:url', urlencodedParser, (req, res) => {
+  const shorturl = req.params.url;
+
+  Url.exists({ shortened_url: parseInt(shorturl) }, (err, result) => {
+    if (err) return console.log(err);
+    else if (result) {
+      Url.find({ shortened_url: parseInt(shorturl) }, (err, data) => {
+        if (err) return console.log(err);
+        return res.redirect(data[0].url);
+      });
+    } else {
+      return res.json({
+        error: 'invalid url'
+      });
+    }
+  });
+
+});
+
+
+
+
